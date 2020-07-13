4e1fe4a2e28b336574b413955c740c2936ade3c5
caixinquan <caixinquan@dfl.com.cn>
2020-07-13 09:54:01 +0800

    feat(index.js): 添加ignoreFiles配置项

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 module.exports = {
   rules: {
     // Disallow unknown type selectors
-    "selector-type-no-unknown": null,
+    'selector-type-no-unknown': null,
 
     // Disallow unknown units
-    "unit-no-unknown": null
+    'unit-no-unknown': null
   },
   extends: [
-    "stylelint-config-standard",
-    "stylelint-config-recess-order"
+    'stylelint-config-standard',
+    'stylelint-config-recess-order'
   ],
-  plugins: ["stylelint-order"]
+  plugins: ['stylelint-order'],
+  ignoreFiles: ['src/libs/**']
 };
